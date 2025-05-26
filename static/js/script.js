6c173df29e4a01e51b3d5bfd26590940f94f33f9
Alex-Shanley <shanleyalex18@yahoo.ie>
2025-05-26 18:55:24 +0100

    My work

---
+++ static/js/script.js
@@ -0,0 +1,21 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const customCursor = document.getElementById('Blog-cursor');
+    const articles = document.querySelectorAll('.article');
+  
+    
+    articles.forEach(article => {
+      article.addEventListener('mouseenter', () => {
+        customCursor.style.opacity = '1';
+      });
+  
+      article.addEventListener('mouseleave', () => {
+        customCursor.style.opacity = '0';
+      });
+    });
+  
+    
+    document.addEventListener('mousemove', (e) => {
+      customCursor.style.left = e.clientX + 'px';
+      customCursor.style.top = e.clientY + 'px';
+    });
+  });
