73f48e1709719c06cd6924c9aede0a4d0051c194
Alex-Shanley <shanleyalex18@yahoo.ie>
2025-05-26 23:21:06 +0100

    work grid

--- static/js/script.js
+++ static/js/script.js
@@ -19,3 +19,26 @@ document.addEventListener('DOMContentLoaded', () => {
       customCursor.style.top = e.clientY + 'px';
     });
   });
+
+
+  document.addEventListener('DOMContentLoaded', () => {
+    const customCursor = document.getElementById('My-work-cursor');
+    const articles = document.querySelectorAll('.work-showcase');
+  
+    
+    articles.forEach(article => {
+      article.addEventListener('mouseenter', () => {
+        customCursor.style.opacity = '1';
+      });
+  
+      article.addEventListener('mouseleave', () => {
+        customCursor.style.opacity = '0';
+      });
+    });
+  
+    
+    document.addEventListener('mousemove', (e) => {
+      customCursor.style.left = e.clientX + 'px';
+      customCursor.style.top = e.clientY + 'px';
+    });
+  });
