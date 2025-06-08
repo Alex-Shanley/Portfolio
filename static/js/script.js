7c912a4592350895f05ecc1243444748c9ff9b2c
Alex-Shanley <shanleyalex18@yahoo.ie>
2025-06-08 18:07:35 +0100

    nav bar update mobile

--- static/js/script.js
+++ static/js/script.js
@@ -85,6 +85,17 @@ navLinks.forEach(link =>
 );
 
 
+function showSidebar(){
+  const sidebar= document.querySelector('.sidebar')
+  sidebar.style.display = 'flex'
+}
+
+function hideSidebar() {
+  const sidebar= document.querySelector('.sidebar')
+  sidebar.style.display = 'none'
+}
+
+
 
 
 
